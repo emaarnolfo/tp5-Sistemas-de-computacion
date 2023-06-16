fda29eeda9429b214fbce442c071d5323da377e5
emaarnolfo <emanuel.arnolfo@mi.unc.edu.ar>
2023-06-16 02:08:44 -0300

    Mejoras en la app

--- EjUltrasonico/read.py
+++ EjUltrasonico/read.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import time 
 import os
 
-x_values = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1]
-y_values = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+x_values = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+y_values = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
 def update_graph(indice):
@@ -13,15 +13,26 @@ def update_graph(indice):
 	trip = int.from_bytes(data, 'little')
 	secs = trip * 1e-6 / 2
 	dist = 340 * secs
-	y_values[indice%10] = dist * 100
+	y_values[indice%9] = dist * 100
 	print(f'Total roundtrip took {trip} us, this gives us a distance of {dist * 100:.2f} cm')
 	
+	plt.clf()	#Limpiar el gráfico anterior
 	plt.plot(x_values, y_values)
 	plt.xlabel('Eje X')
 	plt.ylabel('Eje Y')
 	plt.title('Grafico de Datos')
-	plt.show()
+	plt.pause(0.1)		# Pausa para lograr la actualizacion del grafico
 	
-for i in range(10):
+# Configurar el modo interactivo de Matplolib
+plt.ion()
+
+# Crear la figura
+plt.figure()
+	
+i = 0
+while (1):
 	update_graph(i)
+	i += 1
 	
+plt.ioff()
+plt.show()
